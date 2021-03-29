3887feab9a8d274a9738e737955872cdd091ecdd
Marlen Zhumakov <mzhumakov@gmail.com>
2021-03-29 15:15:13 +0600

    fix DriverSet class

--- src/main/java/DriverSet.java
+++ src/main/java/DriverSet.java
@@ -2,10 +2,15 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class DriverSet {
-    WebDriver driver = new ChromeDriver();
-
-    public DriverSet(WebDriver driver) {
-        this.driver = driver;
+    private static WebDriver driver;
+    
+    private static void createWebDriver() {        
+        if (driver != null) {
+            DriverSet driverSet = new DriverSet();
+            driverSet.getDriver();
+        } else {
+            driver = new ChromeDriver();
+        }
     }
 
     public WebDriver getDriver() {
